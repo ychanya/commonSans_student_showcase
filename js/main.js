ab80ab11f552c3f6edf60d5bfc7f4df9f399d0c5
Hansini Chetan Dhameliya <hansini.dhameliya2@gmail.com>
2025-09-17 19:54:45 -0400

    Update main.js
    This commit adds code that dynamically updates the student portfolio showcase.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("JS file connected");
+
+// Student Portfolio Dynamic Code
+const studentList = document.querySelector("#student-list");
+const students = [
+    {id: 1, name: "Isabella Marie Taylor", roles: ["Graphic","PM"], portfolio: "#"},
+    {id: 2, name: "Ling Zhao", roles: ["Front-End","PM"], portfolio: "#"}
+];
+
+function renderStudents(items) {
+    studentList.innerHTML = items.map(s => `
+        <li>
+            <article>
+                <h4>${s.name}</h4>
+                <ul>${(s.roles||[]).map(r => `<li>${r}</li>`).join('')}</ul>
+            </article>
+        </li>    
+    `).join('');
+}
+
+renderStudents(students);
